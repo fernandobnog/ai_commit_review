9ff16752de03a9371d6cb5baf5e10f6ff8701b15
Fernando Nogueira <fernandonogueira@ntadvogados.com.br>
2024-11-28 10:19:07 -0300

    tentativas de configuração

--- i18n.js
+++ i18n.js
@@ -3,14 +3,28 @@ import path from "path";
 import fs from "fs";
 import { loadConfig } from "./config.js";
 import { ConfigKeys } from "./models.js";
+import { fileURLToPath } from "url";
 
-// Caminho para a pasta locales
-const localesDir = path.join(process.cwd(), "locales");
+// Resolver __dirname para ES Modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Diretório 'locales' no mesmo local do script
+const localesDir = path.resolve(__dirname, "locales");
 
 // Obter idiomas disponíveis na pasta locales
 const getAvailableLocales = () => {
-  const files = fs.readdirSync(localesDir); // Listar arquivos na pasta
-  return files.map((file) => path.parse(file).name); // Extrair nomes sem extensão
+  try {
+    console.log("Diretório de idiomas:", localesDir);
+    const files = fs.readdirSync(localesDir); // Listar arquivos na pasta
+    return files.map((file) => path.parse(file).name); // Extrair nomes sem extensão
+  } catch (error) {
+    console.error(
+      `Erro ao acessar o diretório de idiomas: ${localesDir}`,
+      error
+    );
+    return []; // Retorna array vazio em caso de erro
+  }
 };
 
 const config = loadConfig();
